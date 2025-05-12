cf6b4b219f5f094a46a0be28e43474d4a47110df
mohan2k178 <akshara2k17@gmail.com>
2025-05-12 05:41:12 -0400

    ProductService May 25

---
+++ src/main/java/com/scaler/productservicemay25/Product.java
@@ -0,0 +1,4 @@
+package com.scaler.productservicemay25;
+
+public class Product {
+}
---
+++ src/main/java/com/scaler/productservicemay25/Productservicemay25Application.java
@@ -0,0 +1,13 @@
+package com.scaler.productservicemay25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Productservicemay25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Productservicemay25Application.class, args);
+    }
+
+}
