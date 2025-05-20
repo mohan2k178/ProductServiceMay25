b4bd50b60e731e947149105fc79e72e49c3717fd
mohan2k178 <akshara2k17@gmail.com>
2025-05-20 15:39:39 -0400

    DB Changes

--- src/main/java/com/scaler/productservicemay25/repositories/ProductRepository.java
+++ src/main/java/com/scaler/productservicemay25/repositories/ProductRepository.java
@@ -1,7 +1,30 @@
 package com.scaler.productservicemay25.repositories;
-
+import com.scaler.productservicemay25.model.Category;
+import com.scaler.productservicemay25.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public class ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Override
+    Optional<Product> findById(Long productId);
+
+    List<Product> findByTitleContainsIgnoreCase(String title);
+
+    List<Product> findByPriceBetween(Double priceAfter, Double priceBefore);
+
+    Optional<Product> findByCategory(Category category);
+
+    List<Product> findAllByCategory_Id(Long categoryId);
+
+    List<Product> findAllByCategory_Title(String categoryTitle);
+
+    @Query("select title from products where id=?")
+    Optional<Product> findOnlyProductTitleById(Long productId);
+
 }
+
--- src/main/java/com/scaler/productservicemay25/services/SelfProductService.java
+++ src/main/java/com/scaler/productservicemay25/services/SelfProductService.java
@@ -2,14 +2,23 @@ package com.scaler.productservicemay25.services;
 
 import com.scaler.productservicemay25.exceptions.ProductNotFoundException;
 import com.scaler.productservicemay25.model.Product;
+import com.scaler.productservicemay25.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("selfProductService")
 public class SelfProductService implements ProductService{
+    private ProductRepository productRepository;
+    public SelfProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+
     @Override
     public Product getSingleProduct(Long productId) throws ProductNotFoundException {
+        //return productRepository.findById(productId);
         return null;
     }
 
