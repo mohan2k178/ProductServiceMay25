15483c3062c2b81761665c5187e87c510d166ef1
mohan2k178 <akshara2k17@gmail.com>
2025-05-22 16:30:05 -0400

    Mapped By

--- src/main/java/com/scaler/productservicemay25/model/Category.java
+++ src/main/java/com/scaler/productservicemay25/model/Category.java
@@ -1,13 +1,17 @@
 package com.scaler.productservicemay25.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Entity(name = "categories")
 public class Category extends BaseModel{
     private String title;
-
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
